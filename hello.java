38653ee4eeae1d133a97b254ba3b01958bd0384e
meenal26 <meenalmeenu26@gmail.com>
2017-06-06 14:00:34 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class world {
+
+	public static void main(String[] args) {
+		        int i = 0;
+		        int j = 10;
+
+		   for(i = 1; i <= j; i++){
+
+		          System.out.println("hello world");
+		   }
+	}
+
+}
